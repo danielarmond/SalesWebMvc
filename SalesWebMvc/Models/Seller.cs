260d269b2fb92cccdcf39cf80326ea7f54c5d9e7
Daniel Armond <dani.xavierarmond@gmail.com>
2023-01-24 15:17:11 -0300

    Inclusão de novas entidades

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,41 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(string name, string email, double baseSalary, DateTime birthDate, Department department, ICollection<SalesRecord> sales)
+        {
+            Name = name;
+            this.email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+        Sales.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+        Sales.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
